4f9cdbb05c54fecbc4ca48f0dc9e4686437ef0ba
chenjichuan <chenjichuan521@sina.cn>
2018-09-02 10:56:14 +0800

    Initial Commit

---
+++ pages/edit/edit.js
@@ -0,0 +1,47 @@
+// pages/edit/edit.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  handleClick() {
+    // wx.switchTab({
+    //   url: '/pages/my/my'
+    // })
+    wx.navigateBack({
+      delta: 1
+    })
+  }
+})
---
+++ pages/search/search.js
@@ -0,0 +1,35 @@
+var app=getApp();
+Page({
+  data: {
+    inputShowed: false,
+    inputVal: ""
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+  taphandler(e) {
+    var con = e.currentTarget.dataset;
+    app.globalData.selectCondition = con;
+    wx.switchTab({
+      url: '/pages/location/location'
+    })
+  }
+});
